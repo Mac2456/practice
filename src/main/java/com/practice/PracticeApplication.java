6c95844f2e8858c256c907dc30ac399c010193f0
Mike <lmihir@ymail.com>
2020-10-06 11:47:25 -0400

    6 updated

--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -9,18 +9,10 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
-
-	@Bean
-	public Docket docket() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.practice")).build();
-	}
-
 
 }
